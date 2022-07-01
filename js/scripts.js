8a2f88137d6bbbb1a8c775706dc2052a69d86eb9
Sincere Villanueva III <svillanueva.git@gmail.com>
2022-07-01 13:45:17 -0700

    Add button and function for subsequent user grids Created a function that serves to create the first 16x16 grid, as well as to create the subsequent grids per the user prompt. Also added a function that clears the previous grid. And also added an event listener for when clicking of the Refresh button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
 
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 256; i++) {
-  const gridSquares = document.createElement('div');
-  gridSquares.classList.add('gridSquares');
-  container.appendChild(gridSquares);
+
+function gridSize(num) {
+  num = num*num;
+  for (let i = 0; i < num; i++) {
+    const gridSquares = document.createElement('div');
+    gridSquares.classList.add('gridSquares');
+    container.appendChild(gridSquares);
+  }
 }
 
+
+function removeAllChildNodes() {
+  while (container.firstChild) {
+      container.removeChild(container.firstChild);
+  }
+}
+
+
+const button = document.querySelector('#refresh');
+button.addEventListener('click', () => {
+  newNum = prompt("How many squares on each side?");
+  removeAllChildNodes();
+  gridSize(newNum);
+})
+
+
+gridSize(16);
